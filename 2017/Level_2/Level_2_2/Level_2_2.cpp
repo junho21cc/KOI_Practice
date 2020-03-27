bb1f78c638efbd8712c12c862789c92c4f143a7e
신준호 <junho1018@gmail.com>
2020-03-27 23:48:32 +0900

    곡선자르기.

--- 2017/Level_2/Level_2_2/Level_2_2.cpp
+++ 2017/Level_2/Level_2_2/Level_2_2.cpp
@@ -10,9 +10,12 @@ int main()
     int n;
     int x1, y1, x2, y2 = 0;
 
+    int not_included_by_other_peak_number = 0;
+    int included_other_peak = 0;
+
     cin >> n;
     
-    int mountaintop = 0;
+    int peak = 0;
     int start_end = 0;
 
     for (int i = 0; i < n; i++)
@@ -23,24 +26,24 @@ int main()
         {
             if (y1 > 0)
             {
-                position[mountaintop][0] = x1;
+                position[peak][0] = x1;
                 start_end++;
             }
             else
             {
-                position[mountaintop][1] = x1;
-                if (position[mountaintop][0] > position[mountaintop][1])
+                position[peak][1] = x1;
+                if (position[peak][0] > position[peak][1])
                 {
-                    int change = position[mountaintop][0];
-                    position[mountaintop][0] = position[mountaintop][1];
-                    position[mountaintop][1] = change;
+                    int change = position[peak][0];
+                    position[peak][0] = position[peak][1];
+                    position[peak][1] = change;
                 }
                 start_end++;
             }
 
             if (start_end == 2)
             {
-                mountaintop++;
+                peak++;
                 start_end = 0;
             }
         }
@@ -48,18 +51,18 @@ int main()
 
         if (i == n - 1)
         {
-            if ((position[mountaintop][0] != 0) && (position[mountaintop][1] == 0))
+            if ((position[peak][0] != 0) && (position[peak][1] == 0))
             {
-                position[0][0] = position[mountaintop][1];
+                position[0][0] = position[peak][1];
             }
         }
     }
 
-    for (int i = 0; i < mountaintop; i++)
+    for (int i = 0; i <= peak; i++)
     {
-        for (int k = 1; k <= mountaintop; k++)
+        for (int k = 0; k <= peak; k++)
         {
-            if (position[i][0] > position[k][0])
+            if (position[i][0] < position[k][0])
             {
                 int change_x1;
                 int change_x2;
@@ -76,8 +79,23 @@ int main()
         }
     }
 
+    int check_start_position = position[0][0];
+    int check_end_position = position[0][1];
 
-    for (int i = 0; i < mountaintop; i++)
+   
+    for (int k = 1; k <= peak; k++)
+    {
+        if (check_start_position > position[k][0])
+        {
+            included_other_peak++;
+            check_start_position = position[k][0];
+        }
+
+    }
+  
+
+
+    for (int i = 0; i < peak; i++)
     {
         cout << position[i][0] << " " << position[i][1] << "\n";
     }
@@ -88,5 +106,4 @@ int main()
 /*
 다른 봉우리에 의해 포함되지 않는 봉우리 개수와
 다른 봉우리를 포함하지 않는 봉우리 개수
-
 */
