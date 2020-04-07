645446bc0cdcfa887ee2704dd9122229122d8a83
신준호 <junho1018@gmail.com>
2020-04-07 22:15:57 +0900

    산만한 고양이

---
+++ 2017/Level_2/Level_2_4/Level_2_4.cpp
@@ -0,0 +1,31 @@
+﻿// https://www.acmicpc.net/problem/14866
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+int room[300000];
+int way[300000];
+
+int sum = 0;
+
+int main()
+{
+    int room_number;
+    int way_number;
+
+    cin >> room_number >> way_number;
+  
+    for (int i = 0; i < way_number; i++)
+    {
+        int a1, a2;
+        cin >> a1 >> a2;
+
+        way[a1]++;
+        way[a2]++;
+
+
+    return 0;
+}
