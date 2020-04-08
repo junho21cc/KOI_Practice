6c51633dd4993499f14137e6e37f16c8af77ee9d
신준호 <junho1018@gmail.com>
2020-04-09 01:13:52 +0900

    dfs와 backtracking의 차이를 비교한다

--- 2017/Level_2/Level_2_4/Level_2_4.cpp
+++ 2017/Level_2/Level_2_4/Level_2_4.cpp
@@ -6,78 +6,31 @@
 
 using namespace std;
 
-
-int way[300000];
-
 int biggest = 0;
-int room_numbering = 0;
 
-vector<int> ways[300000];
+int room_number;
+int way_number;
 
-void dfs(int x)
+int ways[300000];
+
+vector<int> way[300000];
+
+void backtracking(int x)
 {
-    if (way[x])
-        return;
-    way[x] = true;
-    cout << x << "\n";
-    for (int k = 0; k < ways[x].size(); k++)
-    {
-        int y = ways[x][k];
-        dfs(y);
-    }
+
 }
 
 int main()
 {
-    int room_number;
-    int way_number;
-
     cin >> room_number >> way_number;
-    
-    /*
-    for (int i = 0; i < way_number; i++)
-    {
-        int a1, a2;
-        cin >> a1 >> a2;
-
-        way[a1]++;
-        way[a2]++;
-    }
-
-    for (int i = 1; i <= room_number; i++)
-    {
-        if (way[i] > biggest)
-        {
-            biggest = way[i];
-            room_numbering = i;
-        }
-    }
-
-    for (int i = 1; i <= room_number; i++)
-    {
-        if (way[i] == biggest)
-        {
-            biggest = 0;
-            break;
-        }
-    }
-
-    cout << biggest;
-    */
-
+   
     for (int k = 0; k < way_number; k++)
     {
         int a1, a2;
         cin >> a1 >> a2;
 
-        ways[a1].push_back(a2);
-        ways[a2].push_back(a1);
+        way[a1].push_back(a2);
+        way[a2].push_back (a1);
     }
-
-    for (int k = 1; k <= room_number; k++)
-    {
-        dfs(k);
-    }
-
     return 0;
 }
